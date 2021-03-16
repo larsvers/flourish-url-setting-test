b43f590a312cc006fa7e38fc949391e0537dd8a1
larsvers <larsverspohl@gmail.com>
2021-03-16 11:25:36 +0000

    Update bar chart colour

--- app.js
+++ app.js
@@ -1,20 +1,45 @@
 function ready() {
+  // Updateing the line chart.
+  // ------------------------
+
   // These are the settings we want to change.
-  const specificSettings = {
+  const lineSettings = {
     series_filter: ['BEN'],
   };
 
   // Encode the settings
-  const encodedSettings = encodeURIComponent(JSON.stringify(specificSettings));
+  const encodedLineSettings = encodeURIComponent(JSON.stringify(lineSettings));
 
   // Get the iframe's src (which we will update)
-  const iFrame = document.querySelector('#my-line-chart iframe');
-  const iFrameURL = new URL(iFrame.src);
-  iFrameURL.hash = encodedSettings;
+  const iFrameLine = document.querySelector('#my-line-chart iframe');
+  const iFrameLineURL = new URL(iFrameLine.src);
+  iFrameLineURL.hash = encodedLineSettings;
 
   // Add the encoded settings to the iframe's src.
   setTimeout(() => {
-    iFrame.src = iFrameURL.href;
+    iFrameLine.src = iFrameLineURL.href;
+  }, 2000);
+
+  // Updateing the bar chart.
+  // -----------------------
+
+  const barSettings = {
+    color: {
+      categorical_custom_palette: 'EGY:orange',
+    },
+  };
+
+  // Encode the settings
+  const encodedBarSettings = encodeURIComponent(JSON.stringify(barSettings));
+
+  // Get the iframe's src (which we will update)
+  const iFrameBar = document.querySelector('#my-bar-chart iframe');
+  const iFrameBarURL = new URL(iFrameBar.src);
+  iFrameBarURL.hash = encodedBarSettings;
+
+  // Add the encoded settings to the iframe's src.
+  setTimeout(() => {
+    iFrameBar.src = iFrameBarURL.href;
   }, 2000);
 }
 
